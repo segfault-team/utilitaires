b50d587692f6cd4ad333e9331345a892e7d0e166
vlistrat <vlistrat@student.42.fr>
2017-01-19 15:39:27 +0100

    test read input

---
+++ redirections/test.c
@@ -0,0 +1,7 @@
+int		main(void)
+{
+	while ((ret = read(0, &buf[++i], 1)))
+	{
+		if ("<") --> enregister nom fichier dans var fichier : open(fichier, O_RDONLY) ; read/write fichier sur 0 avec fonction redirection;
+	}
+}
