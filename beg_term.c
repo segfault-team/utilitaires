be26fcf273e335c07769c8b1bde2a4cfa9af814d
kboddez <kboddez@student.42.fr>
2017-01-21 15:38:21 +0100

    add identification of key code

--- beg_term.c
+++ beg_term.c
@@ -1,6 +1,8 @@
+#include <stdlib.h>
 #include <curses.h>
 #include <term.h>
-#include "libft.h"
+#include <unistd.h>
+#include <stdio.h>
 
 int	putchar_ft(int c)
 {
@@ -10,11 +12,20 @@ int	putchar_ft(int c)
 	return (write(1, &d, 1));
 }
 
+int	get_touch(struct termios termos)
+{
+	char	buf[3];
+	char	*res;
+
+	return (0);
+}
+
 int main()
 {
 	char			*name_term;
 	struct termios	termos;
 	char			*res;
+	char			buf[3];
 
 	if ((name_term = getenv("TERM")) == NULL)
 		return (-1);
@@ -26,12 +37,23 @@ int main()
 	termos.c_lflag &= ~(ECHO);
 	termos.c_cc[VMIN] = 1;
 	termos.c_cc[VTIME] = 0;
-	if ((res = tgetstr("cl", NULL)) == NULL)
-		return (-1);
-	tputs(res, 0, putchar_ft);
 	if (tcsetattr(0, TCSADRAIN, &termos) == -1)
 		return (-1);
 	while (1)
-		;
+	{
+		read(0, buf, 3);
+		if (buf[0] == 4)
+			printf("^d\n");
+		else if (buf[0] == 12)
+		{
+			res = tgetstr("cl", NULL);
+			tputs(res, 0, putchar_ft);
+		}
+		if (buf[0])
+			printf("%d | %d | %d\n", buf[0], buf[1], buf[2]);
+		buf[0] = 0;
+		buf[1] = 0;
+		buf[2] = 0;
+	}
 	return (0);
 }
